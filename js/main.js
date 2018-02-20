99223f0e1c76fcc78b8fa2820708abed782057e6
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2018-02-20 18:46:39 +0100

    add esri/widgets/
    add scalebar and fullscreen widgets

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ define([
     "esri/views/MapView",
     "esri/Map",
     "esri/widgets/BasemapGallery",
+    "esri/widgets/ScaleBar",
+    "esri/widgets/Fullscreen",
     "js/initWidgets",
     "dojo/domReady!"
-], function (MapView, Map, BasemapGallery, initWidgets) {
+], function (MapView, Map, BasemapGallery, ScaleBar, Fullscreen, initWidgets) {
 
     return {
         mapView: null,
@@ -24,16 +26,31 @@ define([
                     basemap: 'streets'
                 }), // Reference to the map object created before the scene
                 zoom: 6, // Sets zoom level based on level of detail (LOD)
-                center: [3.262939, 36.618283] // Sets center point of view using longitude,latitude
+                center: [3.262939, 36.618283], // Sets center point of view using longitude,latitude
+                ui: {
+                    components: ["zoom", "compass", "attribution"]
+                }
             });
 
             var basemapContainer = document.createElement("div");
             $(basemapContainer).addClass("basemapContainer hidden").appendTo("#main");
-
+            //Basemap gallery
             var basemapGallery = new BasemapGallery({
                 view: this.mapView,
                 container: basemapContainer
             });
+            //scalebar widget
+            var scaleBar = new ScaleBar({
+                view: this.mapView
+            });
+            this.mapView.ui.add(scaleBar, "bottom-right");
+            // full screen widget
+            fullscreen = new Fullscreen({
+                view: this.mapView
+            });
+            this.mapView.ui.add(fullscreen, "top-right");
+
+
 
         }
     }
