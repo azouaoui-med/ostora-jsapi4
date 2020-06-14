ae24b6a85e21923c929a5531cc95c3d3ab104ff0
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-06-14 18:05:47 +0100

    add google maps to basemap list

--- src/js/config/nls/ar/local.js
+++ src/js/config/nls/ar/local.js
@@ -1,9 +1,11 @@
 define({
-      Example:"مثال",
-      drawTitleMenu :"رسم",
-      drawTitle :"مثال الرسم",
-      algeria:"ولايات الجزائر",
-      satellite:"صور الأقمار الصناعية",
-      streets:"خريطة الشارع العالمية",
-      osm :"خريطة الشارع المفتوحة"
+  Example: 'مثال',
+  drawTitleMenu: 'رسم',
+  drawTitle: 'مثال الرسم',
+  algeria: 'ولايات الجزائر',
+  satellite: 'صور الأقمار الصناعية',
+  streets: 'خريطة الشارع العالمية',
+  osm: 'خريطة الشارع المفتوحة',
+  googleStreets: 'خريطة جوجل للشارع ',
+  googleSatellite: 'خريطة جوجل للأقمار الصناعية',
 });
--- src/js/config/nls/local.js
+++ src/js/config/nls/local.js
@@ -1,12 +1,14 @@
 define({
-    root: {
-        Example:"Example",
-        drawTitleMenu :"Draw",
-        drawTitle :"Draw Example",
-        algeria:"Algeria states",
-        satellite:"World Imagery",
-        streets:"World Street Map",
-        osm :"Open Street Map"
-    },
-    "ar": true
+  root: {
+    Example: 'Example',
+    drawTitleMenu: 'Draw',
+    drawTitle: 'Draw Example',
+    algeria: 'Algeria states',
+    satellite: 'World Imagery',
+    streets: 'World Street Map',
+    osm: 'Open Street Map',
+    googleStreets: 'Google Streets',
+    googleSatellite: 'Google Satellite',
+  },
+  ar: true,
 });
